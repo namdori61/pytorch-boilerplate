a8bd7c4847fad874a2309867ca7f558cf4d345d4
Sangho.Nam <namdori61@gmail.com>
2020-07-28 12:18:15 +0900

    Trainer: Add hyperparameters (batch_size, num_workers, lr) and gpu usage

--- train.py
+++ train.py
@@ -12,17 +12,36 @@ flags.DEFINE_string('input_path', default=None,
                     help='Path to the training dataset')
 flags.DEFINE_integer('cuda_device', default=0,
                      help='If given, uses this CUDA device in training')
+flags.DEFINE_integer('batch_size', default=4,
+                     help='If given, uses this batch size in training')
+flags.DEFINE_integer('num_workers', default=0,
+                     help='If given, uses this number of workers in data loading')
+flags.DEFINE_float('lr', default=2e-5,
+                   help='If given, uses this learning rate in training')
 
 
 def main(argv):
-    model = Model(input_path=FLAGS.input_path)
+    model = Model(input_path=FLAGS.input_path,
+                  batch_size=FLAGS.batch_size,
+                  num_workers=FLAGS.num_workers,
+                  lr=FLAGS.lr)
 
     # most basic trainer, uses good defaults (1 gpu)
-    trainer = Trainer(gpus=FLAGS.cuda_device)
-    logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
-    logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
+    if FLAGS.cuda_device > 1:
+        trainer = Trainer(gpus=FLAGS.cuda_device,
+                          distributed_backend='ddp',
+                          log_gpu_memory=True)
+        logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
+    elif FLAGS.cuda_device == 1:
+        trainer = Trainer(gpus=FLAGS.cuda_device,
+                          log_gpu_memory=True)
+        logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
+    else:
+        trainer = Trainer()
+        logging.info('No GPU available, using the CPU instead.')
     trainer.fit(model)
-
 
 if __name__ == '__main__':
     flags.mark_flags_as_required([
