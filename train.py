1e92265bf7f2a90be7056b35c5a0543086cc67cc
Sangho.Nam <namdori61@gmail.com>
2020-07-29 15:01:14 +0900

    Train: Add model checkpoint to save model.

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from absl import app, flags, logging
 
 import torch
 from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from model import Model
 
@@ -10,6 +11,8 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('input_path', default=None,
                     help='Path to the training dataset')
+flags.DEFINE_string('save_dir', default=None,
+                    help='Path to save model')
 flags.DEFINE_integer('cuda_device', default=0,
                      help='If given, uses this CUDA device in training')
 flags.DEFINE_integer('batch_size', default=4,
@@ -30,22 +33,34 @@ def main(argv):
     if FLAGS.seed != 0:
         seed_everything(FLAGS.seed)
 
+    checkpoint_callback = ModelCheckpoint(
+        filepath=FLAGS.save_dir,
+        save_top_k=True,
+        verbose=True,
+        monitor='val_loss',
+        mode='min',
+        prefix='model.ckpt'
+    )
+
     # most basic trainer, uses good defaults (1 gpu)
     if FLAGS.cuda_device > 1:
         trainer = Trainer(deterministic=True,
                           gpus=FLAGS.cuda_device,
                           distributed_backend='ddp',
-                          log_gpu_memory=True)
+                          log_gpu_memory=True,
+                          checkpoint_callback=checkpoint_callback)
         logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
         logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
     elif FLAGS.cuda_device == 1:
         trainer = Trainer(deterministic=True,
                           gpus=FLAGS.cuda_device,
-                          log_gpu_memory=True)
+                          log_gpu_memory=True,
+                          checkpoint_callback=checkpoint_callback)
         logging.info(f'There are {torch.cuda.device_count()} GPU(s) available.')
         logging.info(f'Use the number of GPU: {FLAGS.cuda_device}')
     else:
-        trainer = Trainer(deterministic=True)
+        trainer = Trainer(deterministic=True,
+                          checkpoint_callback=checkpoint_callback)
         logging.info('No GPU available, using the CPU instead.')
     trainer.fit(model)
 
